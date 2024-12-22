d6a60679053e1c4b4c68edac3221bd4f2ee1989f
Tarek Oukacha <tarekoukasha2009@gmail.com>
2024-12-22 12:49:22 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,49 @@
+# data types on python
+
+# 1.integer
+age = 80
+money = 6,000
+
+# 2.float
+weight = 63.92
+hight = 180.04
+
+# 3.string
+name = "tarek"
+gender = "male"
+myself = """
+my name is tarek.
+I go to alain english speaaking.
+I live in the united arab emirates.
+I am from syria and germany.
+I have lived in the uae for 8 years now.
+"""
+
+# 4.boolean
+is_male = True
+is_short = False
+
+# 5.list
+colors = ["red","white","green"]
+furniture = ["table","chair","couch"]
+
+# 6.tuple
+ordered_history = ("mobile phone","fridge","computer")
+car_brands = ("mercedes benz","ferrari","genesis")
+
+
+print(f"data type of age is {type(age)}")
+print("data type of age is",type(age))
+
+print(f"data type of money is {type(money)}")
+print(f"data type of weight is {type(weight)}")
+print(f"data type of hight is {type(hight)}")
+print(f"data type of name is {type(name)}")
+print(f"data type of gender is {type(gender)}")
+print(f"data type of myself is {type(myself)}")
+print(f"data type of is_male is {type(is_male)}")
+print(f"data type of is_short is {type(is_short)}")
+print(f"data type of colors is {type(colors)}")
+print(f"data type of furniture is {type(furniture)}")
+print(f"data type of ordered_history is {type(ordered_history)}")
+print(f"data type of car_brands is {type(car_brands)}")
